6c8ba9f732361374629bb1afe419d10c68cff066
sajith <sajithd784@gmail.com>
2022-06-14 09:10:19 +0530

    Finalize the UI of dashbord Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashbordComponent
   ],
   imports: [
     BrowserModule,
