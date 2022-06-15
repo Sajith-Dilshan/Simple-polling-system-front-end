b3a0a1759ba2bf5d56da20e579710683535d859d
sajith <sajithd784@gmail.com>
2022-06-15 14:38:19 +0530

    Finalize dashboard component

--- src/app/dashbord/dashbord.component.ts
+++ src/app/dashbord/dashbord.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../service/user.service";
+import {Router} from "@angular/router";
+import {Poll} from "../dto/poll";
 
 @Component({
   selector: 'app-dashbord',
@@ -7,15 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashbordComponent implements OnInit {
 
-  polls: Array<number> = []
+  polls: Array<Poll> = []
 
-  constructor() { }
+
+  constructor(private userService: UserService,
+              private routerService: Router) { }
 
   ngOnInit(): void {
+    if (!this.userService.getPrincipal()){
+      this.routerService.navigateByUrl('login');
+    }
   }
 
-  createPoll() {
-    this.polls.push(1);
+  createPoll(title: string) {
+    this.polls.push(new Poll(Math.random(), title, this.userService.getPrincipal()!));
   }
 
 }
--- src/app/dto/poll.ts
+++ src/app/dto/poll.ts
@@ -1,6 +1,6 @@
 export class Poll {
 
-  constructor(id: number, title: string, createdBy: string, upVote: number = 0, downVote: number = 0) {
+  constructor(public id: number, public title: string, public createdBy: string,
+              public upVotes: number = 0, public downVotes: number = 0) {
   }
-
 }
